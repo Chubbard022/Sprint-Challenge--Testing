783a44d6f87de2055d424deb61b62c90acbb8615
Chubbard022 <chubbard022@gmail.com>
2019-05-17 08:25:24 -0700

    testing working, mvp met

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,57 +6,64 @@ const dbConfig = require("../knexfile")
 
 const db = knex(dbConfig.development)
 
-describe("server.js",()=>{
-    describe("GET /games",()=>{
-        it("should respond with 200",()=>{
-            const response = await request(server).get("/games")
-            expect(response.status).toBe(200)
-        })
-        it("should respond with giving json",()=>{
-            const response = await request(server).get("/games")
-            expect(response.type).toMatch(/jason/i)
-        })
-        it("should respond with an object",()=>{
-            const response = await request(server).get("/games")
-            expect(response.body).toEqual([])
-        })
-    })
-    describe("POST /games",()=>{
-        afterEach(async ()=>{
-            await db("games").truncate()
-        })
-        it("should respond with 422 if any information is missing",()=>{
-            const body = {
-                title: 'Pacman',
-               releaseYear: 1980 
-            }
-            const response = await request(server)
-                .post("/games")
-                .send(body)
-                expect(response.status).toBe(422)
-                db("games").truncate()
-        })
-        it("should respond with 201",()=>{
-            const body = {
-                 title: 'Pacman',
-                 genre: 'Arcade',
-                releaseYear: 1980 
-            }
-            const response = await request(server)
-                .post("/games")
-                .send(body)
-                expect(response.status).toBe(201)
-        })
-        it("should respond with a returning array",()=>{
-            const body = {
-                title: 'Pacman',
-                 genre: 'Arcade',
-                releaseYear: 1980 
-            }
-            const response = await request(server)
-                .post("/games")
-                .send(body)
-                expect(response.body.length).toBe(1)
-        })
-    })
-})
+describe("api", () => {
+    describe("get /games", () => {
+      it("should respond with 200", async () => {
+        const response = await request(server).get("/games");
+        expect(response.status).toBe(200);
+      });
+  
+      it("should respond with giving json", async () => {
+        const response = await request(server).get("/games");
+        expect(response.type).toMatch(/json/i);
+      });
+  
+      it("should respond with an object", async () => {
+        const response = await request(server).get("/games");
+        expect(response.body).toEqual([]);
+      });
+    });
+  
+    describe("post /games", () => {
+      afterEach(async () => {
+        await db("games").truncate();
+      });
+  
+      it("should respond with 420 if any information is missing", async () => {
+        const body = {
+            title: 'dig dug',
+            releaseYear: 1970
+        };
+        const response = await request(server)
+          .post("/games")
+          .send(body);
+        expect(response.status).toBe(420);
+        db("games").truncate();
+      });
+  
+      it("should respond with 201", async () => {
+        const body = {
+            title: 'dig dug',
+            genre: "Arcade",
+            releaseYear: 1970
+        };
+        const response = await request(server)
+          .post("/games")
+          .send(body);
+        expect(response.status).toBe(201);
+      });
+  
+      it("should respond with a returning array", async () => {
+        const body = {
+            title: 'dig dug',
+            genre: "Arcade",
+            releaseYear: 1970
+        };
+        const response = await request(server)
+          .post("/games")
+          .send(body);
+        expect(response.body.length).toBe(1);
+      });
+    });
+  });
+  
